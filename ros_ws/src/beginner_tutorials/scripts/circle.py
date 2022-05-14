96f4adba96b31573ed106497b3fe97092455f2b8
Scott Hadzik <scotthadzik@weber.edu>
2022-05-13 23:43:22 -0600

    circle simulation

---
+++ ros_ws/src/beginner_tutorials/scripts/circle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Twist
+
+def move():
+    # Starts a new node
+    rospy.init_node('turtle_c', anonymous=True)
+    velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    vel_msg = Twist() 
+    vel_msg.linear.x = 0.5
+    vel_msg.linear.y = 0
+    vel_msg.linear.z = 0
+    vel_msg.angular.x = 0
+    vel_msg.angular.y = 0
+    vel_msg.angular.z = 0.5
+
+    while not rospy.is_shutdown():
+        velocity_publisher.publish(vel_msg)
+
+if __name__ == '__main__':
+    try:
+        #Testing our function
+        move()
+    except rospy.ROSInterruptException: pass
